5d20d435bad29d84969e0bca1b204b9aaba4b587
ahkee <root@DESKTOP-Q6IIDQB.localdomain>
2021-10-25 18:07:16 +0800

    使用新的方式LOAD AND RUN 修改测试用例

--- src/cpu.rs
+++ src/cpu.rs
@@ -26,23 +26,60 @@ impl CPU {
         }
     } 
     // 内存相关的操作
+    
     fn mem_read(&self, addr: u16) -> u8 {
         self.memory[addr as usize]
+    }
+    /*
+    存储地址需要2个字节，6502使用的是小端寻址，
+    这意味着地址的 8 个最低有效位将存储在 8 个最高有效位之前。
+    真实地址：0x8000
+    大端封装的地址：80 00
+    小端封装的地址：00 80
+    */
+    fn mem_read_u16(&mut self, pos:u16) -> u16 {
+        // LDA $8000  <=>  ad 00 80  pos传进来的是 00的内存地址  
+        let lo = self.mem_read(pos) as u16;  // 低位
+        let hi = self.mem_read(pos+1) as u16; // 高位
+        (hi << 8) | (lo as u16)
     }
 
     fn mem_write(&mut self, addr: u16, data:u8){
         self.memory[addr as usize] = data;
     }
 
+    fn mem_write_u16(&mut self, pos: u16, data:u16){
+        // 写2字节的数据，也要小端封装
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xff) as u8;
+        self.mem_write(pos, lo);
+        self.mem_write(pos + 1, hi);
+    }
+
+    /*
+    插入新卡会触发 "Reset interrupt"的特殊信号
+    该信号指示CPU:
+    重置状态（寄存器和标志）
+    将program_counter设置为存储在 0xFFFC 的 16 位地址
+    */
+    pub fn reset(&mut self){
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+        self.program_counter = self.mem_read_u16(0xFFFC);
+    }
+
     pub fn load_and_run(&mut self, program:Vec<u8>){
         self.load(program);
-        self.run() 
+        self.reset();
+        self.run();
     }
 
     pub fn load(&mut self, program:Vec<u8>){
         // 将ROM LOAD 到内存 0x8000开始
         self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
-        self.program_counter = 0x8000; // PC指向ROM的开始地址，然后执行程序
+        // self.program_counter = 0x8000; // PC指向ROM的开始地址，然后执行程序
+        self.mem_write_u16(0xFFFC, 0x8000);
     }
 
 
@@ -50,15 +87,15 @@ impl CPU {
     // 1. 从指令寄存器中获取下一条执行命令
     // 解码指令-> 执行指令-> 重复循环
     // program 是内存器
-    pub fn interpret(&mut self, program: Vec<u8>){
-        self.program_counter = 0;
+    pub fn run(&mut self){
+        // 运行ROM中的代码, 这是通过内存的方式读取
         loop {
-            let opscode = program[self.program_counter as usize];
+            let opscode = self.mem_read(self.program_counter);
             self.program_counter += 1;
 
             match opscode {
                 0xA9 =>{ // LDA指令为两字节， 一字节是操作码本身，一字节是参数
-                    let param = program[self.program_counter as usize];
+                    let param = self.mem_read(self.program_counter);
                     self.program_counter += 1;
                     self.register_a = param; // 将参数LOAD 到 累加器A上
                     // 更新 处理器状态寄存器P的 bit 1 - Zero Flag and bit 7 - Negative Flag
@@ -80,18 +117,6 @@ impl CPU {
                 _ => todo!()
             }
 
-        }
-        
-    }
-
-    pub fn run(&mut self){
-        // 运行ROM中的代码, 这是通过内存的方式读取
-        loop {
-            let opscode = self.mem_read(self.program_counter);
-            self.program_counter += 1;
-            match opscode {
-                _ => todo!()
-            }
         }
 
     }
@@ -119,7 +144,7 @@ mod test{
     #[test]
     fn test_0xa9_lda_immidate_load_data(){
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
         assert_eq!(cpu.register_a, 0x05);
         assert!(cpu.status & 0b0000_0010 == 0b0000_0000);
         assert!(cpu.status & 0b1000_0000 == 0b0000_0000);
@@ -128,40 +153,40 @@ mod test{
     #[test]
     fn test_0xa9_lda_zero_flag() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
         assert!(cpu.status & 0b0000_0010 == 0b0000_0010);
     }
 
     #[test]
     fn test_0xa9_lda_negative_flag() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0b1100_0000, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0b1100_0000, 0x00]);
         assert!(cpu.status & 0b1000_0000 == 0b1000_0000);
     }
 
     #[test]
     fn test_0xaa_tax_move_a_to_x(){
         let mut cpu = CPU::new();
-        cpu.register_a = 10;
-        cpu.interpret(vec![0xaa, 0x00]);
+        // cpu.register_a = 10;  LDA #$10 TAX
+        cpu.load_and_run(vec![0xa9, 0x0a, 0xaa, 0x00]);
         assert_eq!(cpu.register_x, 10);
     }
-
-    #[test]
-   fn test_5_ops_working_together() {
-       let mut cpu = CPU::new();
-       cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
- 
-       assert_eq!(cpu.register_x, 0xc1)
-   }
 
    #[test]
    fn test_inx_overflow() {
        let mut cpu = CPU::new();
-       cpu.register_x = 0xff;
-       cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+       // cpu.register_x = 0xff;
+       cpu.load_and_run(vec![0xa9, 0xff, 0xe8, 0x00]);
 
        assert_eq!(cpu.register_x, 1)
+   }
+
+   #[test]
+   fn test_5_ops_working_together() {
+       let mut cpu = CPU::new();
+       cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+
+       assert_eq!(cpu.register_x, 0xc1)
    }
 
 }
